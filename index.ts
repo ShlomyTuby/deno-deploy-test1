96a443c5b60da77f54c520f12c337dc45180a8ec
Shalom Tuby <shlomy.tuby@gmail.com>
2021-12-29 21:40:52 +0200

    SGA-9999  some code

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello world")
+import { listenAndServe } from "https://deno.land/std@0.111.0/http/mod.ts";
+
+// Create a pattern that matches paths starting with /books/, followed by an integer.
+const pattern = new URLPattern({ pathname: "/books/:id(\\d+)" });
+
+function handler(req: Request): Response {
+    const url = req.url;
+
+    const match = pattern.exec(url);
+    if (match) {
+        console.log(match.pathname);
+        const bookId = match.pathname.groups.id;
+        return new Response(`Book: ${bookId}`)
+    }
+
+    return new Response("Not Found\nTry /books/123", { status: 404 });
+}
+
+await listenAndServe(":8080", handler);
